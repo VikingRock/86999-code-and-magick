c9d1f4ee5cf9d9b800d1818a8c1906d84a92dcb4
VikingRock <a.zinchuk.a@gmail.com>
2016-01-20 11:59:17 +0300

    check.js - getMessage() added

--- js/check.js
+++ js/check.js
@@ -1,9 +1,39 @@
 'use strict';
 
-(function() {
+function getMessage(a, b) {
 
-  function getMessage(a, b) {
+  var message;
 
+  if ( (typeof a) == 'boolean' ) {
+    (a) ? (message = 'Я попал в ' + b) : (message = 'Я никуда не попал');
+    return message;
   }
 
-})();
+  if ( (typeof a) == 'number' ) {
+    message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+    return message;
+  }
+
+  if ( Array.isArray(a) && !Array.isArray(b) ) {
+    var sumArray = a.reduce(function(x, y) {
+      return x + y;
+    });
+
+    message = 'Я прошёл ' + sumArray + ' шагов';
+    return message;
+  }
+
+  if ( Array.isArray(a) && Array.isArray(b) ) {
+    var multiply = a.map( function(currentValue, index) {
+      return currentValue * b[index];
+    });
+
+    var length = multiply.reduce(function(x, y) {
+      return x + y;
+    });
+
+    message = 'Я прошёл ' + length + ' метров';
+    return message;
+  }
+
+}
