3c35370ab5811f93f25c3025654d8d67bce8038f
VikingRock <a.zinchuk.a@gmail.com>
2016-01-27 23:36:34 +0300

    required check added for name and feedback

--- js/form.js
+++ js/form.js
@@ -5,6 +5,13 @@
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
 
+  var form = document.forms[0];
+  var reviewBlock = document.querySelector('.review-fields');
+  var reviewLabels = document.querySelectorAll('.review-fields-label');
+  var nameField = document.getElementById('review-name');
+  var feedbackField = document.getElementById('review-text');
+  var rateFields = document.querySelectorAll('[name=review-mark]');
+
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible');
@@ -14,4 +21,21 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  function makeRequired() {
+    if ( rateFields[0].checked || rateFields[1].checked ) {
+      feedbackField.required = true;
+    } else {
+      feedbackField.required = false;
+    }
+  }
+
+  nameField.required = true;
+
+  for (var i = 0; i < rateFields.length; i++) {
+    rateFields[i].onchange = function() {
+      makeRequired();
+    }
+  }
+
 })();
