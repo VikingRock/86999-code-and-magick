130087ef8c7e8ef664c9944517a944a5cca577fc
VikingRock <a.zinchuk.a@gmail.com>
2016-01-31 16:56:28 +0300

    building testimonials from <template>

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,21 @@
 'use strict';
 
 (function() {
-  console.log(window.reviews);
-})()
+  var filterBlock = document.querySelector('.reviews-filter');
+  filterBlock.classList.add('invisible');
+
+  window.reviews.forEach(function(testimonial) {
+    var element = getElementFromTemplate(testimonial);
+    filterBlock.appendChild(element);
+  });
+
+  function getElementFromTemplate(templateData) {
+    var testimonialTemplate = document.getElementById('review-template');
+    var element = testimonialTemplate.content.childNodes[1].cloneNode(true);
+
+    return element;
+  }
+
+  filterBlock.classList.remove('invisible');
+
+})();
