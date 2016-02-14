97647a688b5c340ed894e655d366f15a8a3d2c5d
VikingRock <a.zinchuk.a@gmail.com>
2016-02-14 22:11:32 +0300

    gallery.hide() added

--- js/gallery.js
+++ js/gallery.js
@@ -4,14 +4,22 @@
 
   var Gallery = function() {
     this.element = document.querySelector('.overlay-gallery');
+    this._closeButton = document.querySelector('.overlay-gallery-close');
+    this._onCloseClick = this._onCloseClick.bind(this);
   };
 
   Gallery.prototype.show = function() {
     this.element.classList.remove('invisible');
+    this._closeButton.addEventListener('click', this._onCloseClick);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
+    this._closeButton.removeEventListener('click', this._onCloseClick);
+  };
+
+  Gallery.prototype._onCloseClick = function() {
+    this.hide();
   };
 
   window.Gallery = Gallery;
