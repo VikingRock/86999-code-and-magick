c6e94de4612ed0a01be2585b0f0a1e16108201f3
VikingRock <a.zinchuk.a@gmail.com>
2016-02-14 22:24:05 +0300

    esc added

--- js/gallery.js
+++ js/gallery.js
@@ -6,20 +6,31 @@
     this.element = document.querySelector('.overlay-gallery');
     this._closeButton = document.querySelector('.overlay-gallery-close');
     this._onCloseClick = this._onCloseClick.bind(this);
+    this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
   };
 
   Gallery.prototype.show = function() {
     this.element.classList.remove('invisible');
+
     this._closeButton.addEventListener('click', this._onCloseClick);
+    document.addEventListener('keypress', this._onDocumentKeyDown);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
+
     this._closeButton.removeEventListener('click', this._onCloseClick);
+    document.removeEventListener('keypress', this._onDocumentKeyDown);
   };
 
   Gallery.prototype._onCloseClick = function() {
     this.hide();
+  };
+
+  Gallery.prototype._onDocumentKeyDown = function(evt) {
+    if (evt.keyCode == 27) {
+      this.hide();
+    }
   };
 
   window.Gallery = Gallery;
