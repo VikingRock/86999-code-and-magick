649966bd0606c2a16ee4d32c6154484957e93b09
VikingRock <a.zinchuk.a@gmail.com>
2016-02-14 14:48:09 +0300

    Review constructor and photo added

--- js/reviews.js
+++ js/reviews.js
@@ -1,3 +1,5 @@
+/* global Review: true */
+
 'use strict';
 
 (function() {
@@ -112,8 +114,9 @@
     }
 
     pageReviews.forEach(function(testimonial) {
-      var element = getElementFromTemplate(testimonial);
-      fragment.appendChild(element);
+      var reviewElement = new Review(testimonial);
+      reviewElement.render();
+      fragment.appendChild(reviewElement.element);
     });
 
     reviewsList.appendChild(fragment);
@@ -122,7 +125,7 @@
   //load review list by XHR
   function getReviews() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://o0.github.io/assets/json/reviews.json');
+    xhr.open('GET', 'https://o0.github.io/assets/json/reviews.json');
     xhr.timeout = 10000;
 
     xhr.onload = function(evt) {
@@ -151,49 +154,6 @@
     }
 
     xhr.send();
-  }
-
-  //create a new DOM element - review - from template and return this element
-  function getElementFromTemplate(templateData) {
-    var testimonialTemplate = document.getElementById('review-template');
-    var element;
-
-    if ('content' in testimonialTemplate) {
-      element = testimonialTemplate.content.childNodes[1].cloneNode(true);
-    } else {
-      element = testimonialTemplate.childNodes[1].cloneNode(true);
-    }
-
-    element.querySelector('.review-text').textContent = templateData.description;
-    element.querySelector('.review-author').setAttribute('alt', templateData.author.name);
-    element.querySelector('.review-author').setAttribute('title', templateData.author.name);
-
-    for (var i = 0; i < templateData.rating - 1; i++) {
-      var ratingClone = element.querySelector('.review-rating').cloneNode();
-      element.insertBefore(ratingClone, element.querySelector('.review-rating'));
-    }
-
-    var authorAvatar = new Image(124, 124);
-    var imageLoadTimeout;
-    var IMAGE_TIMEOUT = 1000;
-
-    authorAvatar.onload = function() {
-      clearTimeout(imageLoadTimeout);
-      element.querySelector('.review-author').setAttribute('src', authorAvatar.src);
-    };
-
-    authorAvatar.onerror = function() {
-      element.classList.add('review-load-failure');
-    };
-
-    authorAvatar.src = templateData.author.picture;
-
-    imageLoadTimeout = setTimeout( function() {
-      element.querySelector('.review-author').setAttribute('src', '');
-      element.classList.add('review-load-failure');
-    }, IMAGE_TIMEOUT);
-
-    return element;
   }
 
   getReviews();
