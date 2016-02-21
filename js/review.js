270e07912a272ec208b16889cb5415b071d36dc3
VikingRock <a.zinchuk.a@gmail.com>
2016-02-21 18:17:35 +0300

    setUsefulness added

--- js/review.js
+++ js/review.js
@@ -4,7 +4,47 @@
 
   function Review(data) {
     this._data = data;
+    this._onUsefulClick = this._onUsefulClick.bind(this);
   }
+
+  Review.prototype.setUsefulness = function(flag) {
+    var yes = this.element.querySelector('.review-quiz-answer-yes');
+    var no = this.element.querySelector('.review-quiz-answer-no');
+    var delta = 1;
+
+    if (this.element.querySelector('.review-quiz-answer-active')) {
+      delta = 2;
+    }
+
+    if (flag) {
+      if (yes.classList.contains('review-quiz-answer-active')) {
+        return;
+      }
+      this._data.review_usefulness += delta;
+      yes.classList.add('review-quiz-answer-active');
+      no.classList.remove('review-quiz-answer-active');
+
+    } else {
+      if (no.classList.contains('review-quiz-answer-active')) {
+        return;
+      }
+      this._data.review_usefulness -= delta;
+      no.classList.add('review-quiz-answer-active');
+      yes.classList.remove('review-quiz-answer-active');
+    }
+  };
+
+  //click on review usefulness yes/no
+  Review.prototype._onUsefulClick = function(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('review-quiz-answer')) {
+      if (evt.target.classList.contains('review-quiz-answer-yes')) {
+        this.setUsefulness(true);
+      } else {
+        this.setUsefulness(false);
+      }
+    }
+  };
 
   //create a new DOM element - review - from template and return this element
   Review.prototype.render = function() {
@@ -19,6 +59,8 @@
     this.element.querySelector('.review-text').textContent = this._data.description;
     this.element.querySelector('.review-author').setAttribute('alt', this._data.author.name);
     this.element.querySelector('.review-author').setAttribute('title', this._data.author.name);
+
+    this.element.addEventListener('click', this._onUsefulClick);
 
     for (var i = 0; i < this._data.rating - 1; i++) {
       var ratingClone = this.element.querySelector('.review-rating').cloneNode();
