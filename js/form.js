9d4799b3962ff83f4f207c980fd7b425be24765b
VikingRock <a.zinchuk.a@gmail.com>
2016-02-28 14:59:25 +0300

    fixed CommonJS modules
    deleted wrapping anonymous functions

--- js/form.js
+++ js/form.js
@@ -2,153 +2,150 @@
 
 'use strict';
 
-(function() {
-  var formContainer = document.querySelector('.overlay-container');
-  var formOpenButton = document.querySelector('.reviews-controls-new');
-  var formCloseButton = document.querySelector('.review-form-close');
+var formContainer = document.querySelector('.overlay-container');
+var formOpenButton = document.querySelector('.reviews-controls-new');
+var formCloseButton = document.querySelector('.review-form-close');
 
-  var form = document.forms[1];
-  var submitForm = document.querySelector('.review-submit');
-  var reviewBlock = document.querySelector('.review-fields');
-  var reviewLabels = document.querySelectorAll('.review-fields-label');
-  var nameField = document.getElementById('review-name');
-  var feedbackField = document.getElementById('review-text');
-  var rateFields = document.querySelectorAll('[name=review-mark]');
+var form = document.forms[1];
+var submitForm = document.querySelector('.review-submit');
+var reviewBlock = document.querySelector('.review-fields');
+var reviewLabels = document.querySelectorAll('.review-fields-label');
+var nameField = document.getElementById('review-name');
+var feedbackField = document.getElementById('review-text');
+var rateFields = document.querySelectorAll('[name=review-mark]');
 
-  /**
-   * Function makes Feedback required if Rate <=2
-   * invokes checkRequired function
-   */
-  function makeRequired() {
+/**
+ * Function makes Feedback required if Rate <=2
+ * invokes checkRequired function
+ */
+function makeRequired() {
 
-    feedbackField.required = false;
+  feedbackField.required = false;
 
-    if ( rateFields[0].checked || rateFields[1].checked ) {
-      feedbackField.required = true;
-    }
-
-    var boundCheck = checkRequired.bind(feedbackField);
-    boundCheck();
+  if ( rateFields[0].checked || rateFields[1].checked ) {
+    feedbackField.required = true;
   }
 
-  /**
-   * shows/hides labels for required fields;
-   * invokes checkSubmit function
-   */
-  function checkRequired() {
-    var identifier = this.id;
-    var targetLabel = document.querySelector('[for=' + identifier + '].review-fields-label');
+  var boundCheck = checkRequired.bind(feedbackField);
+  boundCheck();
+}
 
-    if ( (this.required) && (this.value !== '') || (!this.required) ) {
-      targetLabel.classList.add('invisible');
-    } else {
-      targetLabel.classList.remove('invisible');
-    }
+/**
+ * shows/hides labels for required fields;
+ * invokes checkSubmit function
+ */
+function checkRequired() {
+  var identifier = this.id;
+  var targetLabel = document.querySelector('[for=' + identifier + '].review-fields-label');
 
-    checkSubmit();
-  }
-
-  /**
-   * checks if all required fields are filled
-   * if true, enables submit button
-   */
-  function checkSubmit() {
-    for (var i = 0; i < reviewLabels.length; i++) {
-      if ( !reviewLabels[i].classList.contains('invisible') ) {  //if a required field is empty
-        submitForm.disabled = true;
-        reviewBlock.classList.remove('invisible');
-        return;
-      }
-
-      reviewBlock.classList.add('invisible');
-      submitForm.disabled = false;
-    }
-  }
-
-
-  /**
-   * check rate and make feedback required if rate <=2
-   */
-  for (var i = 0; i < rateFields.length; i++) {
-    rateFields[i].onchange = function() {
-      makeRequired();
-    };
-  }
-
-  /**
-   * check if required fields are not empty
-   */
-  nameField.oninput = checkRequired;
-  feedbackField.oninput = checkRequired;
-
-  /**
-   * form open and close event
-   */
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.remove('invisible');
-  };
-
-  formCloseButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.add('invisible');
-  };
-
-  /**
-   * form submit event
-   */
-  form.onsubmit = function(event) {
-    event.preventDefault();
-
-    /**
-     * @const {string}
-     */
-    var MY_BIRTHDAY = '1988 10 26';
-    var myBDate = new Date(MY_BIRTHDAY);
-    var now = new Date();
-
-    if ( ( now.getMonth() === myBDate.getMonth() && now.getDate() >= myBDate.getDate() ) || now.getMonth() > myBDate.getMonth() ) {
-      myBDate.setFullYear(now.getFullYear());
-    } else {
-      myBDate.setFullYear(now.getFullYear() - 1);
-    }
-
-    var cookieLife = new Date( now - myBDate + now.valueOf() ).toUTCString();
-    var currentRate = document.querySelector('input[name="review-mark"]:checked').value;
-
-    docCookies.setItem('rate', currentRate, cookieLife);
-    docCookies.setItem('name', nameField.value, cookieLife);
-
-    form.submit();
-  };
-
-  /**
-   * default settings
-   */
-
-  nameField.required = true;
-  reviewLabels[1].classList.add('invisible');
-
-  /**
-   * setting cookies if exist
-   */
-  var name = docCookies.getItem('name');
-  var rate = docCookies.getItem('rate');
-
-  if (name && rate) {
-    nameField.value = docCookies.getItem('name');
-    var defaultRate = docCookies.getItem('rate');
-
-    document.getElementById('review-mark-' + defaultRate).checked = true;
-    reviewLabels[0].classList.add('invisible');
-    reviewBlock.classList.add('invisible');
-    /**
-     * function call to check fields and set them required if needed
-     */
-    makeRequired();
-
+  if ( (this.required) && (this.value !== '') || (!this.required) ) {
+    targetLabel.classList.add('invisible');
   } else {
-    submitForm.disabled = true;
+    targetLabel.classList.remove('invisible');
   }
 
-})();
+  checkSubmit();
+}
+
+/**
+ * checks if all required fields are filled
+ * if true, enables submit button
+ */
+function checkSubmit() {
+  for (var i = 0; i < reviewLabels.length; i++) {
+    if ( !reviewLabels[i].classList.contains('invisible') ) {  //if a required field is empty
+      submitForm.disabled = true;
+      reviewBlock.classList.remove('invisible');
+      return;
+    }
+
+    reviewBlock.classList.add('invisible');
+    submitForm.disabled = false;
+  }
+}
+
+
+/**
+ * check rate and make feedback required if rate <=2
+ */
+for (var i = 0; i < rateFields.length; i++) {
+  rateFields[i].onchange = function() {
+    makeRequired();
+  };
+}
+
+/**
+ * check if required fields are not empty
+ */
+nameField.oninput = checkRequired;
+feedbackField.oninput = checkRequired;
+
+/**
+ * form open and close event
+ */
+formOpenButton.onclick = function(evt) {
+  evt.preventDefault();
+  formContainer.classList.remove('invisible');
+};
+
+formCloseButton.onclick = function(evt) {
+  evt.preventDefault();
+  formContainer.classList.add('invisible');
+};
+
+/**
+ * form submit event
+ */
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+  /**
+   * @const {string}
+   */
+  var MY_BIRTHDAY = '1988 10 26';
+  var myBDate = new Date(MY_BIRTHDAY);
+  var now = new Date();
+
+  if ( ( now.getMonth() === myBDate.getMonth() && now.getDate() >= myBDate.getDate() ) || now.getMonth() > myBDate.getMonth() ) {
+    myBDate.setFullYear(now.getFullYear());
+  } else {
+    myBDate.setFullYear(now.getFullYear() - 1);
+  }
+
+  var cookieLife = new Date( now - myBDate + now.valueOf() ).toUTCString();
+  var currentRate = document.querySelector('input[name="review-mark"]:checked').value;
+
+  docCookies.setItem('rate', currentRate, cookieLife);
+  docCookies.setItem('name', nameField.value, cookieLife);
+
+  form.submit();
+};
+
+/**
+ * default settings
+ */
+
+nameField.required = true;
+reviewLabels[1].classList.add('invisible');
+
+/**
+ * setting cookies if exist
+ */
+var name = docCookies.getItem('name');
+var rate = docCookies.getItem('rate');
+
+if (name && rate) {
+  nameField.value = docCookies.getItem('name');
+  var defaultRate = docCookies.getItem('rate');
+
+  document.getElementById('review-mark-' + defaultRate).checked = true;
+  reviewLabels[0].classList.add('invisible');
+  reviewBlock.classList.add('invisible');
+  /**
+   * function call to check fields and set them required if needed
+   */
+  makeRequired();
+
+} else {
+  submitForm.disabled = true;
+}
--- js/review.js
+++ js/review.js
@@ -1,119 +1,117 @@
 'use strict';
 
-(function() {
+/**
+ * creates a Review object and renders it to the page
+ * @constructor
+ * @param {Object} data
+ */
+function Review(data) {
+  this._data = data;
+  this._data.reviewed = 'n/a';
+  this._onUsefulClick = this._onUsefulClick.bind(this);
+}
+
+/**
+ * modifies usefulness score for Review
+ * and displays user's choice
+ * @param {Boolean} flag
+ */
+Review.prototype.setUsefulness = function(flag) {
+  var yes = this.element.querySelector('.review-quiz-answer-yes');
+  var no = this.element.querySelector('.review-quiz-answer-no');
+  var delta = 1;
+
   /**
-   * creates a Review object and renders it to the page
-   * @constructor
-   * @param {Object} data
+   * if user has already clicked before
+   * set delta = 2
    */
-  function Review(data) {
-    this._data = data;
-    this._data.reviewed = 'n/a';
-    this._onUsefulClick = this._onUsefulClick.bind(this);
+  if (this._data.reviewed !== 'n/a') {
+    delta = 2;
   }
 
-  /**
-   * modifies usefulness score for Review
-   * and displays user's choice
-   * @param {Boolean} flag
-   */
-  Review.prototype.setUsefulness = function(flag) {
-    var yes = this.element.querySelector('.review-quiz-answer-yes');
-    var no = this.element.querySelector('.review-quiz-answer-no');
-    var delta = 1;
-
-    /**
-     * if user has already clicked before
-     * set delta = 2
-     */
-    if (this._data.reviewed !== 'n/a') {
-      delta = 2;
+  if (flag) {
+    if (this._data.reviewed === 'yes') {
+      return;
     }
+    this._data.review_usefulness += delta;
+    yes.classList.add('review-quiz-answer-active');
+    no.classList.remove('review-quiz-answer-active');
+    this._data.reviewed = 'yes';
 
-    if (flag) {
-      if (this._data.reviewed === 'yes') {
-        return;
-      }
-      this._data.review_usefulness += delta;
-      yes.classList.add('review-quiz-answer-active');
-      no.classList.remove('review-quiz-answer-active');
-      this._data.reviewed = 'yes';
+  } else {
+    if (this._data.reviewed === 'no') {
+      return;
+    }
+    this._data.review_usefulness -= delta;
+    no.classList.add('review-quiz-answer-active');
+    yes.classList.remove('review-quiz-answer-active');
+    this._data.reviewed = 'no';
+  }
+};
 
+/**
+ * when review usefulness yes/no clicked
+ * call setUsefulness function
+ * @param {Event} evt
+ */
+Review.prototype._onUsefulClick = function(evt) {
+  evt.preventDefault();
+  var cl = evt.target.classList;
+  if (cl.contains('review-quiz-answer')) {
+    if (cl.contains('review-quiz-answer-yes')) {
+      this.setUsefulness(true);
     } else {
-      if (this._data.reviewed === 'no') {
-        return;
-      }
-      this._data.review_usefulness -= delta;
-      no.classList.add('review-quiz-answer-active');
-      yes.classList.remove('review-quiz-answer-active');
-      this._data.reviewed = 'no';
+      this.setUsefulness(false);
     }
-  };
+  }
+};
 
+/**
+ * render a new DOM element - review -
+ * from template and get its content from _data object
+ */
+Review.prototype.render = function() {
+  var testimonialTemplate = document.getElementById('review-template');
+
+  if ('content' in testimonialTemplate) {
+    this.element = testimonialTemplate.content.childNodes[1].cloneNode(true);
+  } else {
+    this.element = testimonialTemplate.childNodes[1].cloneNode(true);
+  }
+
+  this.element.querySelector('.review-text').textContent = this._data.description;
+  this.element.querySelector('.review-author').setAttribute('alt', this._data.author.name);
+  this.element.querySelector('.review-author').setAttribute('title', this._data.author.name);
+
+  this.element.addEventListener('click', this._onUsefulClick);
+
+  for (var i = 0; i < this._data.rating - 1; i++) {
+    var ratingClone = this.element.querySelector('.review-rating').cloneNode();
+    this.element.insertBefore(ratingClone, this.element.querySelector('.review-rating'));
+  }
+
+  var authorAvatar = new Image(124, 124);
+  var imageLoadTimeout;
   /**
-   * when review usefulness yes/no clicked
-   * call setUsefulness function
-   * @param {Event} evt
+   * @type {number}
    */
-  Review.prototype._onUsefulClick = function(evt) {
-    evt.preventDefault();
-    var cl = evt.target.classList;
-    if (cl.contains('review-quiz-answer')) {
-      if (cl.contains('review-quiz-answer-yes')) {
-        this.setUsefulness(true);
-      } else {
-        this.setUsefulness(false);
-      }
-    }
-  };
+  var IMAGE_TIMEOUT = 1000;
 
-  /**
-   * render a new DOM element - review -
-   * from template and get its content from _data object
-   */
-  Review.prototype.render = function() {
-    var testimonialTemplate = document.getElementById('review-template');
+  authorAvatar.onload = function() {
+    clearTimeout(imageLoadTimeout);
+    this.element.querySelector('.review-author').setAttribute('src', authorAvatar.src);
+  }.bind(this);
 
-    if ('content' in testimonialTemplate) {
-      this.element = testimonialTemplate.content.childNodes[1].cloneNode(true);
-    } else {
-      this.element = testimonialTemplate.childNodes[1].cloneNode(true);
-    }
+  authorAvatar.onerror = function() {
+    this.element.classList.add('review-load-failure');
+  }.bind(this);
 
-    this.element.querySelector('.review-text').textContent = this._data.description;
-    this.element.querySelector('.review-author').setAttribute('alt', this._data.author.name);
-    this.element.querySelector('.review-author').setAttribute('title', this._data.author.name);
+  authorAvatar.src = this._data.author.picture;
 
-    this.element.addEventListener('click', this._onUsefulClick);
+  imageLoadTimeout = setTimeout( function() {
+    this.element.querySelector('.review-author').setAttribute('src', '');
+    this.element.classList.add('review-load-failure');
+  }.bind(this), IMAGE_TIMEOUT);
+};
 
-    for (var i = 0; i < this._data.rating - 1; i++) {
-      var ratingClone = this.element.querySelector('.review-rating').cloneNode();
-      this.element.insertBefore(ratingClone, this.element.querySelector('.review-rating'));
-    }
-
-    var authorAvatar = new Image(124, 124);
-    var imageLoadTimeout;
-    /**
-     * @type {number}
-     */
-    var IMAGE_TIMEOUT = 1000;
-
-    authorAvatar.onload = function() {
-      clearTimeout(imageLoadTimeout);
-      this.element.querySelector('.review-author').setAttribute('src', authorAvatar.src);
-    }.bind(this);
-
-    authorAvatar.onerror = function() {
-      this.element.classList.add('review-load-failure');
-    }.bind(this);
-
-    authorAvatar.src = this._data.author.picture;
-
-    imageLoadTimeout = setTimeout( function() {
-      this.element.querySelector('.review-author').setAttribute('src', '');
-      this.element.classList.add('review-load-failure');
-    }.bind(this), IMAGE_TIMEOUT);
-  };
-
-  module.exports = Review;
-})();
+module.exports = Review;
